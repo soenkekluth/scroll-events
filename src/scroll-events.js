1210b73fe57b8f7a5db4f52da85abbe1b2089d3b
Sönke Kluth <mail@soenkekluth.com>
2016-09-08 20:45:54 +0200

    bump deps && cleanup

--- src/scroll-events.js
+++ src/scroll-events.js
@@ -6,26 +6,23 @@ const _instanceMap = {};
 
 export default class ScrollEvents extends EventDispatcher {
 
-  static getInstance = (_scrollTarget, options) => {
-    if (!_instanceMap[_scrollTarget]) {
-      _instanceMap[_scrollTarget] = new ScrollEvents(_scrollTarget, options);
+  static getInstance(scrollTarget, options){
+    if (!_instanceMap[scrollTarget]) {
+      _instanceMap[scrollTarget] = new ScrollEvents(scrollTarget, options);
     }
-    return _instanceMap[_scrollTarget];
-  };
+    return _instanceMap[scrollTarget];
+  }
 
-  static hasInstance = (_scrollTarget) => {
-    return (typeof _instanceMap[_scrollTarget] !== 'undefined');
-  };
+  static hasInstance(scrollTarget){
+    return (typeof _instanceMap[scrollTarget] !== 'undefined');
+  }
 
   static hasScrollTarget = ScrollEvents.hasInstance;
 
-  static clearInstance = (_scrollTarget = window) => {
-    if (ScrollEvents.hasInstance(_scrollTarget)) {
-      ScrollEvents.getInstance(_scrollTarget).destroy();
-      delete _instanceMap[_scrollTarget];
-    }
-  };
 
+  static get windowScrollY() {
+    return  (window.pageYOffset || window.scrollY || 0);
+  }
 
   static get documentHeight() {
     return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
@@ -35,11 +32,10 @@ export default class ScrollEvents extends EventDispatcher {
     return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);
   }
 
-
-  static unprefixAnimationFrame = () => {
+  static unprefixAnimationFrame(){
     window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     window.cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame || window.msCancelAnimationFrame;
-  };
+  }
 
   static UP = -1;
   static DOWN = 1;
@@ -90,6 +86,12 @@ export default class ScrollEvents extends EventDispatcher {
       ScrollEvents.unprefixAnimationFrame();
     }
 
+    this.init();
+
+  }
+
+  init() {
+
     this._destroyed = false;
     this._scrollY = 0;
     this._scrollX = 0;
@@ -105,12 +107,6 @@ export default class ScrollEvents extends EventDispatcher {
     this._directionX = ScrollEvents.NONE;
     this._scrolling = false;
 
-    this.init();
-
-  }
-
-  init() {
-
     this.getScrollPosition = (this._scrollTarget === window) ? delegate(this, this._getWindowScrollPosition) : delegate(this, this._getElementScrollPosition);
 
     this.onScroll = delegate(this, this.onScroll);
@@ -121,7 +117,7 @@ export default class ScrollEvents extends EventDispatcher {
 
     if (this._scrollTarget.addEventListener) {
       // this._scrollTarget.addEventListener('mousewheel', this.onScroll, Can.passiveEvents ? { passive: true } : false);
-      this._scrollTarget.addEventListener('scroll', this.onScroll, Can.passiveEvents ? { passive: true } : false);
+      this._scrollTarget.addEventListener('scroll', this.onScroll, false);
       this._scrollTarget.addEventListener('resize' ,this.onResize, false);
     } else if (this._scrollTarget.attachEvent) {
       // this._scrollTarget.attachEvent('onmousewheel', this.onScroll);
@@ -168,18 +164,6 @@ export default class ScrollEvents extends EventDispatcher {
       this._scrollTarget = null;
       this._destroyed = true;
     }
-  }
-
-  get attr() {
-    return {
-      scrollY: this.scrollY,
-      // scrollX: this.scrollX,
-      speedY: this.speedY,
-      // speedX: this.speedX,
-      // angle: 0,
-      directionY: this.directionY
-        // directionX: this.directionX
-    };
   }
 
 
@@ -280,14 +264,14 @@ export default class ScrollEvents extends EventDispatcher {
 
   _getWindowScrollPosition() {
     return {
-      y: (window.pageYOffset || window.scrollY || 0),
+      y: ScrollEvents.windowScrollY
       // x: (window.pageXOffset || window.scrollX || 0)
     }
   }
 
   _getElementScrollPosition() {
     return {
-      y: this._scrollTarget.scrollTop,
+      y: this._scrollTarget.scrollTop
       // x: this._scrollTarget.scrollLeft
     }
   }
@@ -301,7 +285,7 @@ export default class ScrollEvents extends EventDispatcher {
     this._currentStopFrames = 0;
     if (this._firstRender) {
       this._firstRender = false;
-      if (this.scrollY > 1) {
+      if (this.y > 1) {
 
         this.updateScrollPosition();
         this.dispatchEvent(ScrollEvents.EVENT_SCROLL_PROGRESS);
@@ -318,9 +302,8 @@ export default class ScrollEvents extends EventDispatcher {
       } else {
         clearTimeout(this._timeout);
         this.onNextFrame();
-        var self = this;
-        this._timeout = setTimeout(function() {
-          self.onScrollStop();
+        this._timeout = setTimeout(() => {
+          this.onScrollStop();
         }, 100);
       }
     }
@@ -334,22 +317,13 @@ export default class ScrollEvents extends EventDispatcher {
     // this._speedX = this._scrollX - this.scrollX;
 
 
-
-    // if(this.options.animationFrame && this._scrolling && ((this._scrollY === this.scrollY ) && (this._lastSpeed === 0 && this.speedY === 0) && (this.directionY === this._lastDirection) && (++this._currentStopFrames > this._stopFrames) /*&& this.directionY === this._lastDirection*/) ){
-    //   this.onScrollStop();
-    //   return;
-    // }
-
-
     if (this.options.animationFrame && (this._scrolling && (this.speedY === 0 && (this._currentStopFrames++ > this._stopFrames)))) {
       this.onScrollStop();
       return;
     }
 
-
     this.updateScrollPosition();
 
-    // console.log(this._lastDirection, this.directionY);
     if (this._lastDirection !== this.directionY) {
       this.dispatchEvent('scroll:' + ScrollEvents.directionToString(this.directionY));
     }
@@ -368,18 +342,16 @@ export default class ScrollEvents extends EventDispatcher {
 
     this.dispatchEvent(ScrollEvents.EVENT_SCROLL_STOP);
 
-    if (this.scrollY <= 0) {
+    if (this.y <= 0) {
       this.dispatchEvent(ScrollEvents.EVENT_SCROLL_TOP);
     } else {
 
 
-      if (this.scrollY + this.clientHeight >= this.scrollHeight) {
+      if (this.y + this.clientHeight >= this.scrollHeight) {
         this.dispatchEvent(ScrollEvents.EVENT_SCROLL_BOTTOM);
       }
     }
 
-    // this.dispatchEvent('scroll:none');
-    // this._scrollX = this.scrollX;
     if (this.options.animationFrame) {
       this._cancelNextFrame();
       this._currentStopFrames = 0;
@@ -389,35 +361,33 @@ export default class ScrollEvents extends EventDispatcher {
 
   _cancelNextFrame() {
     window.cancelAnimationFrame(this.nextFrameID);
-    this.nextFrameID = 0;
+    this.nextFrameID = -1;
   }
 
 }
 
 
-var passiveEvents = null;
+// var passiveEvents = null;
 
 class Can {
-
-
   static get animationFrame() {
     return !!(window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame);
   };
 
 
-  static get passiveEvents() {
-    if (passiveEvents !== null) {
-      return passiveEvents;
-    }
-    try {
-      var opts = Object.defineProperty({}, 'passive', {
-        get: () => {
-          passiveEvents = true;
-        }
-      });
-      window.addEventListener("test", null, opts);
-    } catch (e) {
-      passiveEvents = false;
-    }
-  }
+  // static get passiveEvents() {
+  //   if (passiveEvents !== null) {
+  //     return passiveEvents;
+  //   }
+  //   try {
+  //     var opts = Object.defineProperty({}, 'passive', {
+  //       get: () => {
+  //         passiveEvents = true;
+  //       }
+  //     });
+  //     window.addEventListener("test", null, opts);
+  //   } catch (e) {
+  //     passiveEvents = false;
+  //   }
+  // }
 }
